af3b541af6351b70a8f8f64dbe2701d17de14acf
castrocabellojuanmanuel <jmcastro@iespuntadelverde.es>
2020-11-10 19:06:47 +0100

    first commit

---
+++ src/ciudades.js
@@ -0,0 +1,132 @@
+db.ciudades.insertMany([
+    {
+        Ciudad: "Berlin",
+        Pais: "Alemania",
+        Habitantes: 3769000,
+        Monumentos:["Puerta de Brandeburgo","Muro de Berlin","Catedral de Berlin","Checkpoint Charlie"],
+        TiempoActual:{Temperatura: 7,Viento: 13, Humedad: 90 },
+
+    },
+
+    {
+        Ciudad: "Paris",
+        Pais: "Francia",
+        Habitantes: 2148000,
+        Monumentos:["Torre Eiffel","Museo del Louvre","Catedral de Notre Dame","Opera Garnier"],
+        TiempoActual:{Temperatura: 16,Viento: 6, Humedad: 79 },
+
+    },
+
+    {
+        Ciudad: "Madrid",
+        Pais: "España",
+        Habitantes: 6642000,
+        Monumentos:["Puerta del Sol","Plaza Mayor","Puerta de Alcala","Gran Via"],
+        TiempoActual:{Temperatura: 14,Viento: 3, Humedad: 77 },
+
+    },
+
+    {
+        Ciudad: "Sevilla",
+        Pais: "España",
+        Habitantes: 1950000,
+        Monumentos:["Catedral de Sevilla","Plaza de España","La Giralda","Archivo de Indias"],
+        TiempoActual:{Temperatura: 20,Viento: 3, Humedad: 64 },
+
+    },
+
+    {
+        Ciudad: "Roma",
+        Pais: "Italia",
+        Habitantes: 2873000,
+        Monumentos:["El Coliseo","Fontana de Trevi","San Pedro Del Vaticano","El Panteon"],
+        TiempoActual:{Temperatura: 11,Viento: 6, Humedad: 90 },
+
+    },
+
+    {
+        Ciudad: "Munich",
+        Pais: "Alemania",
+        Habitantes: 1472000,
+        Monumentos:["Marienplatz","Odeonplatz","BMW Welt","Olympiapark"],
+        TiempoActual:{Temperatura: 3,Viento: 6, Humedad: 98 },
+
+    },
+
+    {
+        Ciudad: "Atenas",
+        Pais: "Grecia",
+        Habitantes: 664046,
+        Monumentos:["Delfos","Acropolis","Partenon","Plaka"],
+        TiempoActual:{Temperatura: 16,Viento: 14, Humedad: 68 },
+
+    },
+
+    {
+        Ciudad: "Florencia",
+        Pais: "Italia",
+        Habitantes: 382258,
+        Monumentos:["Catedral de Santa Maria","Ponte Vecchio","David de Miguel Angel","Galeria Uffizi"],
+        TiempoActual:{Temperatura: 8,Viento: 3, Humedad: 90 },
+
+    },
+
+    {
+        Ciudad: "Lisboa",
+        Pais: "Portugal",
+        Habitantes: 504718,
+        Monumentos:["Castillo de San Jorge","Elevador de Santa Justa","Torre de Belem","Plaza del Comercio"],
+        TiempoActual:{Temperatura: 13,Viento: 3, Humedad: 94 },
+
+    },
+
+    {
+        Ciudad: "Moscu",
+        Pais: "Rusia",
+        Habitantes: 11920000,
+        Monumentos:["Kremlin","Plaza Roja","Catedral de San Basilio","Catedral de Kazan"],
+        TiempoActual:{Temperatura: 3,Viento: 21, Humedad: 81 },
+
+    },
+
+    {
+        Ciudad: "Marsella",
+        Pais: "Francia",
+        Habitantes: 861635,
+        Monumentos:["Opera de Marsella","Palacio Longchamp","Abadia de San Victor","Castillo de If"],
+        TiempoActual:{Temperatura: 13,Viento: 3, Humedad: 90 },
+
+    },
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+])
